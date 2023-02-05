7dcf8408fdd81c1bc95c5cf9dfb715915226a046
samanta2403 <89295518+samanta2403@users.noreply.github.com>
2023-02-05 14:23:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function SwitchOn(){
+    document.getElementById('bulb').src='https://s3-projectsassets.whjr.online/On+Bulb.png'
+   }
+   
+   function SwitchOff(){ 
+    document.getElementById('bulb').src='https://s3-projectsassets.whjr.online/Off+Bulb.png'
+   }
